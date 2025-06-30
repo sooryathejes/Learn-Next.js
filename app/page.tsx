493a4c921333260fd13101d9de43789cdb0a2b10
sooryathejes <sooryathjs007@gmail.com>
2025-06-30 23:45:14 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Link href="/dashboard">Dash Board</Link>
-    </div>
-  );
+async function getData() {
+  const data = await prisma
+  return items
 }
+
+export default async function Home() {
+ 
+  return ( 
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tighter mb-8">
+        Latest Posts
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">  
+          {data.map((item)=> ( 
+            <h1 key={item.title}>{item.title}</h1>
+          ))} 
+      </div>
+    </div> 
+  ); 
+} 
